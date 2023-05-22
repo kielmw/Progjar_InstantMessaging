843ccc750b6291c2036804503cd1004cdf5e5b73
kielmw <75736236+kielmw@users.noreply.github.com>
2023-05-22 21:16:52 +0700

    Upload 1

---
+++ InstantMessaging/src/Master/Message.java
@@ -0,0 +1,32 @@
+package Master;
+
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private String sender;
+    private String receiver;
+    private String content;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+    }
+
+    public Message(String sender, String receiver, String content) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
